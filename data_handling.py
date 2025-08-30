a59ba75121a73920082290ab3c4957b273507aa0
shadmehr <ssszzzast@gmail.com>
2025-08-31 00:58:58 +0430

    Docs: add physical model
    Feat: add new function
    new function for get seasonal anime
    
    Refactor: remove some duplicate

--- data_handling.py
+++ data_handling.py
@@ -2,56 +2,112 @@ import requests
 import json
 from time import sleep
 
+#take client id
 __client_id: str
 with open('client id.txt') as file:
-    client_id = file.readline().rstrip('\n')
+    __client_id = file.readline().rstrip('\n')
 
 
-def get_anime_info(anime_id: int, save_data: bool) -> dict:
-    if type(anime_id) != int or type(save_data) != bool:
-        raise TypeError
-    
-    response = requests.get(url=f'https://api.myanimelist.net/v2/anime/{str(anime_id)}', 
-                            headers={'X-MAL-CLIENT-ID' : client_id}, 
-                            params={'fields': 'id,alternative_titles,status,genres,num_episodes,start_season,source,average_episode_duration,rating,studios'}
+#inner functions:
+
+#sending request to mal
+def __send_request(url_add: str, parameters: dict) -> dict | int:
+    response = requests.get(url=f'https://api.myanimelist.net/v2/{url_add}', 
+                            headers={'X-MAL-CLIENT-ID' : __client_id}, 
+                            params=parameters
                             )
     
     result: dict = response.json()
-    if response.status_code != 200:
+    
+    return result, response.status_code
+
+#saving data in a json file
+def __save_data(result: dict) -> None:
+    with open('temp_json.json', 'w') as file:
+            json.dump(result, file, indent=4)
+
+
+
+#get anime information with using his anime id
+def get_anime_info(anime_id: int, save_data: bool) -> dict:
+    #valid type checking
+    if type(anime_id) != int or type(save_data) != bool:
+        raise TypeError('invalid input')
+    
+    #send request
+    url_add: str = f'anime/{str(anime_id)}'
+    parameters: dict = {'fields': 'id,alternative_titles,status,genres,num_episodes,start_season,source,average_episode_duration,rating,studios'}
+    result: dict
+    status_code: int
+    result, status_code = __send_request(url_add, parameters)
+    
+    #check response
+    if status_code != 200:
         raise KeyError('anime dont exist')
     else:
         anime_name: str = result['alternative_titles']['en']
         if len(anime_name) == 0: anime_name = result['title']
         print(f'anime {anime_name} find with {anime_id} ID')
     
-    if save_data:
-        with open('temp_json.json', 'w') as file:
-            json.dump(result, file, indent=4)
+    #saving or not saving data
+    if save_data:__save_data(result)
     
-    
-    sleep(1)
+    #a delay for rest
+    sleep(0.1)
     return result
 
-
+#get list of a account by using his user name
 def get_user_list(user_name: str, save_data: bool) -> dict:
+    #valid type checking
     if type(user_name) != str or type(save_data) != bool:
-        raise TypeError
+        raise TypeError('invalid input')
     
-    response = requests.get(url=f'https://api.myanimelist.net/v2/users/{user_name}/animelist',
-                            headers={'X-MAL-CLIENT-ID' : client_id},
-                            params={'fields': 'list_status', 'limit': 300}
-                            )
+    #send request
+    url_add: str = f'users/{user_name}/animelist'
+    parameters: dict = {'fields': 'list_status', 'limit': 300}
+    result: dict
+    status_code: int
+    result, status_code = __send_request(url_add, parameters)
     
-    result: dict = response.json()
-    if response.status_code != 200:
+    #check response
+    if status_code == 403:
+        raise ConnectionRefusedError(f'{user_name} list is private')
+    elif status_code != 200:
         raise KeyError("user dont exist")
     else:
         print(f'user {user_name} find')
     
-    if save_data:
-        with open('temp_json.json', 'w') as file:
-            json.dump(result, file, indent=4)
+    #saving or not saving data
+    if save_data:__save_data(result)
     
-    sleep(1)
+    #a delay for rest
+    sleep(0.1)
     return result
 
+#get all animes from a season
+def get_seasonal_animes(season: str, year: int, save_data: bool) -> dict:
+    #valid type checking
+    if type(season) != str or type(year) != int or type(save_data) != bool:
+        raise TypeError("invalid input")
+    if season not in ['spring', 'summer', 'fall', 'winter']:
+        raise TypeError("invalid season")
+    if year not in range(1930, 2027):
+        raise TypeError("invalid year")
+    
+    #send request
+    url_add: str = f'anime/season/{str(year)}/{season}'
+    parameters: dict = {'fields': 'id,alternative_titles,status,genres,num_episodes,start_season,source,average_episode_duration,rating,studios'}
+    result: dict
+    status_code: int
+    result, status_code = __send_request(url_add, parameters)
+    
+    #check response
+    if status_code != 200:
+        raise KeyError('seasonal animes dont exist')
+    
+    #saving or not saving data
+    if save_data:__save_data(result)
+    
+    #a delay for rest
+    sleep(0.1)
+    return result
---
+++ database design/physical model.sql
@@ -0,0 +1,62 @@
+CREATE TABLE IF NOT EXISTS account(
+    account_id INT PRIMARY KEY,
+    user_name VARCHAR(20) NOT NULL UNIQUE,
+    plan_watch_count INT DEFAULT 0,
+    completed_count INT DEFAULT 0,
+    dropped_count INT DEFAULT 0,
+    on_hold_count INT DEFAULT 0,
+    watching_count INT DEFAULT 0,
+    avg_score FLOAT(4, 2) DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS anime(
+    anime_id INT PRIMARY KEY,
+    anime_name VARCHAR(100) NOT NULL,
+    anime_status ENUM('currently_airing', 'not_yet_aired', 'finished_airing') NOT NULL,
+    episodes INT DEFAULT 0,
+    year DATE NOT NULL,
+    season ENUM('spring', 'summer', 'fall', 'winter') NOT NULL,
+    avg_episodes_time INT NOT NULL,
+    plan_watch_count INT DEFAULT 0,
+    completed_count INT DEFAULT 0,
+    dropped_count INT DEFAULT 0,
+    on_hold_count INT DEFAULT 0,
+    watching_count INT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS genre(
+    genre_id INT PRIMARY KEY,
+    genre_name VARCHAR(30) NOT NULL,
+    anime_numbers INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS studio(
+    studio_id INT PRIMARY KEY,
+    studio_name VARCHAR(30) NOT NULL,
+    product_numbers INT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS anime_production_studio(
+    anime_id INT PRIMARY KEY,
+    studio_id INT PRIMARY KEY,
+    FOREIGN KEY(anime_id) REFERENCES anime(anime_id) ON DELETE CASCADE,
+    FOREIGN KEY(studio_id) REFERENCES studio(studio_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF EXISTS anime_genres(
+    anime_id INT PRIMARY KEY,
+    genre_id INT PRIMARY KEY,
+    FOREIGN KEY(anime_id) REFERENCES anime(anime_id) ON DELETE CASCADE,
+    FOREIGN KEY(genre_name) REFERENCES genre(genre_name) ON DELETE CASCADE
+);
+
+CREATE TABLE IF EXISTS list(
+    anime_id INT PRIMARY KEY,
+    account_id INT PRIMARY KEY,
+    FOREIGN KEY(anime_id) REFERENCES anime(anime_id) ON DELETE CASCADE,
+    FOREIGN KEY(account_id) REFERENCES account(account_id) ON DELETE CASCADE,
+    score FLOAT(4, 2),
+    CONSTRAINT score_range CHECK (score BETWEEN 10.0 AND 0.0),
+    status ENUM('plan to watch', 'completed', 'dropped', 'on hold', 'watching'),
+    episodes_watched INT DEFAULT 0
+);
