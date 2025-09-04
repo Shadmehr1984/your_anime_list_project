5acb9f1bb6efd7f23e083fb7b52dc2787738f6ca
shadmehr <ssszzzast@gmail.com>
2025-09-05 01:23:30 +0430

    Feat:add log_list table

--- database design/physical model.sql
+++ database design/physical model.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS account(
     account_id INT PRIMARY KEY,
     user_name VARCHAR(20) NOT NULL UNIQUE,
-    plan_watch_count INT DEFAULT 0,
+    plan_to_watch_count INT DEFAULT 0,
     completed_count INT DEFAULT 0,
     dropped_count INT DEFAULT 0,
     on_hold_count INT DEFAULT 0,
@@ -13,11 +13,13 @@ CREATE TABLE IF NOT EXISTS anime(
     anime_id INT PRIMARY KEY,
     anime_name VARCHAR(100) NOT NULL,
     anime_status ENUM('currently_airing', 'not_yet_aired', 'finished_airing') NOT NULL,
+    score FLOAT(4, 2) NOT NULL DEFAULT 0,
+    CONSTRAINT score_range CHECK (score BETWEEN 10.0 AND 0.0),
     episodes INT DEFAULT 0,
     year DATE NOT NULL,
     season ENUM('spring', 'summer', 'fall', 'winter') NOT NULL,
     avg_episodes_time INT NOT NULL,
-    plan_watch_count INT DEFAULT 0,
+    plan_to_watch_count INT DEFAULT 0,
     completed_count INT DEFAULT 0,
     dropped_count INT DEFAULT 0,
     on_hold_count INT DEFAULT 0,
@@ -43,19 +45,31 @@ CREATE TABLE IF NOT EXISTS anime_production_studio(
     FOREIGN KEY(studio_id) REFERENCES studio(studio_id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF EXISTS anime_genres(
+CREATE TABLE IF NOT EXISTS anime_genres(
     anime_id INT PRIMARY KEY,
     genre_id INT PRIMARY KEY,
     FOREIGN KEY(anime_id) REFERENCES anime(anime_id) ON DELETE CASCADE,
     FOREIGN KEY(genre_name) REFERENCES genre(genre_name) ON DELETE CASCADE
 );
 
-CREATE TABLE IF EXISTS list(
+CREATE TABLE IF NOT EXISTS list(
     anime_id INT PRIMARY KEY,
     account_id INT PRIMARY KEY,
     FOREIGN KEY(anime_id) REFERENCES anime(anime_id) ON DELETE CASCADE,
     FOREIGN KEY(account_id) REFERENCES account(account_id) ON DELETE CASCADE,
-    score FLOAT(4, 2),
+    score FLOAT(4, 2) NOT NULL DEFAULT 0,
+    CONSTRAINT score_range CHECK (score BETWEEN 10.0 AND 0.0),
+    status ENUM('plan to watch', 'completed', 'dropped', 'on hold', 'watching'),
+    episodes_watched INT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS log_list(
+    log_status ENUM('insert', 'delete', 'update') NOT NULL,
+    anime_id INT PRIMARY KEY,
+    account_id INT PRIMARY KEY,
+    FOREIGN KEY(anime_id) REFERENCES anime(anime_id) ON DELETE CASCADE,
+    FOREIGN KEY(account_id) REFERENCES account(account_id) ON DELETE CASCADE,
+    score FLOAT(4, 2) NOT NULL DEFAULT 0,
     CONSTRAINT score_range CHECK (score BETWEEN 10.0 AND 0.0),
     status ENUM('plan to watch', 'completed', 'dropped', 'on hold', 'watching'),
     episodes_watched INT DEFAULT 0
