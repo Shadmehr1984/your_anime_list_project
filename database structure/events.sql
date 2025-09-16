4e94c892123d9f72f632d4cc08a334992529e537
shadmehr <ssszzzast@gmail.com>
2025-09-17 02:20:20 +0430

    Feat: add events file and create a event clear log_list every 10 minute

---
+++ database structure/events.sql
@@ -0,0 +1,11 @@
+DELIMITER //
+    -- create event for clear log_list every 10 minute
+    DROP EVENT IF EXISTS clear_log_list;
+
+    
+    CREATE EVENT clear_log_list
+    ON SCHEDULE
+    EVERY 10 MINUTE
+    DO
+        CALL calculate_and_clear_log_list();
+DELIMITER ;
